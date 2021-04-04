94dc6a662c7a4ce9e36f3098c0ac6bcabc0f1cd8
gezp <zhenpeng.ge@qq.com>
2021-04-04 10:09:24 +0800

    add RobotiqController plugin

--- launch/ur10_robotiq140_ign.launch.py
+++ launch/ur10_robotiq140_ign.launch.py
@@ -1,4 +1,4 @@
-'''Launch ur10 ignition_simulator with ros joint trajectory controller and state publisher'''
+'''Launch ur10 ignition_simulator with ros joint position controller and state publisher'''
 
 import os
 
@@ -14,7 +14,7 @@ def generate_launch_description():
     pkg_ros_ign_gazebo = get_package_share_directory('ros_ign_gazebo')
     pkg_universal_robot_ign = get_package_share_directory('universal_robot_ign')
     #data
-    world_sdf_path=os.path.join(pkg_universal_robot_ign, 'worlds', 'ur10_world.sdf') 
+    world_sdf_path=os.path.join(pkg_universal_robot_ign, 'worlds', 'ur10_robotiq140_world.sdf') 
     ign_config_path=os.path.join(pkg_universal_robot_ign, 'ign', 'gui.config')
     # ignition_simulator launch
     ignition_simulator = IncludeLaunchDescription(
@@ -22,7 +22,7 @@ def generate_launch_description():
             os.path.join(pkg_ros_ign_gazebo, 'launch', 'ign_gazebo.launch.py'),
         ),
         launch_arguments={
-            'ign_args': world_sdf_path + ' -v 2 --gui-config ' + ign_config_path,
+            'ign_args': world_sdf_path + ' -v 4 --gui-config ' + ign_config_path,
         }.items()
     )
     # parameter for ur10 controller
@@ -30,7 +30,7 @@ def generate_launch_description():
                     "wrist_1_joint","wrist_2_joint","wrist_3_joint"]
     ign_cmd_joint_topics_list=[]
     for joint_name in joint_names_list:
-        ign_cmd_joint_topics_list.append("/model/ur10/joint/ur10::%s/0/cmd_pos"%joint_name)
+        ign_cmd_joint_topics_list.append("/model/ur10/joint/%s/0/cmd_pos"%joint_name)
     
     # ros<-ign, joint state publisher for ur10
     joint_state_publisher=Node(package='universal_robot_ign', 
@@ -42,15 +42,14 @@ def generate_launch_description():
                         ],
                 output='screen') 
     #  ros->ign,  joint controller for ur10
-    joint_trajectory_controller=Node(package='universal_robot_ign', 
-                executable='joint_trajectory_controller',
-                name="ur10_joint_trajectory_controller",
+    joint_position_controller=Node(package='universal_robot_ign', 
+                executable='joint_position_controller',
+                name="ur10_joint_position_controller",
                 parameters=[{"joint_names": joint_names_list},
                             {"ign_cmd_joint_topics": ign_cmd_joint_topics_list},
-                            {"rate":200},
                            ],
                 output='screen') 
                 
     return LaunchDescription([
-        ignition_simulator,joint_state_publisher,joint_trajectory_controller
+        ignition_simulator,joint_state_publisher,joint_position_controller
     ])
--- plugins/robotiq_controller/RobotiqController.cpp
+++ plugins/robotiq_controller/RobotiqController.cpp
@@ -8,26 +8,26 @@
  *  If not, see <https://opensource.org/licenses/MIT/>.
  *
  ******************************************************************************/
-#include <mutex>
-#include <ignition/common/Util.hh>
-#include <ignition/common/Profiler.hh>
-#include <ignition/plugin/Register.hh>
-#include <ignition/transport/Node.hh>
+#include "RobotiqController.hh"
 
-#include <ignition/gazebo/components/Pose.hh>
-#include <ignition/gazebo/components/LinearVelocity.hh>
-#include <ignition/gazebo/components/AngularVelocity.hh>
+#include <ignition/common/Profiler.hh>
+#include <ignition/common/Util.hh>
+#include <ignition/gazebo/Conversions.hh>
 #include <ignition/gazebo/Link.hh>
 #include <ignition/gazebo/Model.hh>
 #include <ignition/gazebo/Util.hh>
-#include <ignition/gazebo/Conversions.hh>
-
-#include <ignition/math/Vector3.hh>
+#include <ignition/gazebo/components/JointForceCmd.hh>
+#include <ignition/gazebo/components/JointPosition.hh>
+#include <ignition/gazebo/components/JointVelocity.hh>
+#include <ignition/gazebo/components/JointVelocityCmd.hh>
+#include <ignition/gazebo/components/Pose.hh>
+#include <ignition/math/PID.hh>
 #include <ignition/math/Pose3.hh>
 #include <ignition/math/Quaternion.hh>
-#include <ignition/math/PID.hh>
-
-#include "RobotiqController.hh"
+#include <ignition/math/Vector3.hh>
+#include <ignition/plugin/Register.hh>
+#include <ignition/transport/Node.hh>
+#include <mutex>
 
 #define WHEEL_NUM 4
 using namespace ignition;
@@ -35,64 +35,60 @@ using namespace gazebo;
 using namespace systems;
 using namespace std;
 
-
-enum class GripperStatus { keep_close,
+enum class GripperStatus {
+    keep_close,
     keep_open,
     move_close,
-    move_open };
+    move_open
+};
 
-class ignition::gazebo::systems::RobotiqControllerPrivate
-{
-public:
+class ignition::gazebo::systems::RobotiqControllerPrivate {
+   public:
+   public:
+    double getJointPosition(ignition::gazebo::EntityComponentManager &_ecm);
+    void setJointVelocity(ignition::gazebo::EntityComponentManager &_ecm, double vel);
+    bool checkGrasp(ignition::gazebo::EntityComponentManager &_ecm);
+    void OnCmd(const ignition::msgs::Boolean &_msg);
 
-public:
-    void OnCmd(const ignition::msgs::Boolean  &_msg);
-
-public:
+   public:
     transport::Node node;
     //model
     Model model{kNullEntity};
     //gripper joint
-    vector<string> jointNames{"finger_joint","left_inner_knuckle_joint","left_inner_finger_joint",
-                    "right_outer_knuckle_joint","right_inner_knuckle_joint","right_inner_finger_joint"};
+    vector<string> jointNames{"finger_joint", "left_inner_knuckle_joint", "left_inner_finger_joint",
+                              "right_outer_knuckle_joint", "right_inner_knuckle_joint", "right_inner_finger_joint"};
     vector<Entity> jointEntities;
-    vector<int> jointMultipliers{1,-1,1,-1,-1,1};
+    vector<int> jointMultipliers{1, -1, 1, -1, -1, 1};
     vector<ignition::math::PID> jointPids;
-    //base link and grasp link(for contact detection and effort control)
+    //grasp link(for contact detection,not used currently)
     Entity gripperFingerLink[2];
-    Entity gripperBaseLink;
     //gripper cmd
     std::mutex statusMutex;
     GripperStatus gripperStatus = GripperStatus::keep_open;
 };
 
 /******************implementation for RobotiqController************************/
-RobotiqController::RobotiqController() : dataPtr(std::make_unique<RobotiqControllerPrivate>())
-{
-
+RobotiqController::RobotiqController() : dataPtr(std::make_unique<RobotiqControllerPrivate>()) {
 }
 
 void RobotiqController::Configure(const Entity &_entity,
-                             const std::shared_ptr<const sdf::Element> &_sdf,
-                             EntityComponentManager &_ecm,
-                             EventManager & /*_eventMgr*/)
-{
+                                  const std::shared_ptr<const sdf::Element> &_sdf,
+                                  EntityComponentManager &_ecm,
+                                  EventManager & /*_eventMgr*/) {
     this->dataPtr->model = Model(_entity);
-    if (!this->dataPtr->model.Valid(_ecm))
-    {
+    if (!this->dataPtr->model.Valid(_ecm)) {
         ignerr << "RobotiqController plugin should be attached to a model entity. Failed to initialize." << std::endl;
         return;
     }
     // Get params from SDF
-    // Get BaseLink
-    this->dataPtr->gripperBaseLink=this->dataPtr->model.LinkByName(_ecm, "robotiq_arg2f_base_link");
-    this->dataPtr->gripperFingerLink[0]==this->dataPtr->model.LinkByName(_ecm, "left_inner_finger");
-    this->dataPtr->gripperFingerLink[1]==this->dataPtr->model.LinkByName(_ecm, "right_inner_finger");
+    // Get Gripper Finger Link (not used!)
+    //this->dataPtr->gripperFingerLink[0] == this->dataPtr->model.LinkByName(_ecm, "left_inner_finger");
+    //this->dataPtr->gripperFingerLink[1] == this->dataPtr->model.LinkByName(_ecm, "right_inner_finger");
     // Get joint Entity
-    for(auto &joint_name:this->dataPtr->jointNames){
-        auto entity=this->dataPtr->model.JointByName(_ecm, joint_name);
-        if(entity == kNullEntity){
-            ignerr << "joint with name[" << joint_name<< "] not found. " << std::endl;
+    for (auto &joint_name : this->dataPtr->jointNames) {
+        auto entity = this->dataPtr->model.JointByName(_ecm, joint_name);
+        if (entity == kNullEntity) {
+            ignerr << "joint with name[" << joint_name << "] not found. " << std::endl;
             return;
         }
         this->dataPtr->jointEntities.push_back(entity);
@@ -101,57 +97,107 @@ void RobotiqController::Configure(const Entity &_entity,
     std::string topic{this->dataPtr->model.Name(_ecm) + "/gripper"};
     this->dataPtr->node.Subscribe(topic, &RobotiqControllerPrivate::OnCmd, this->dataPtr.get());
     ignmsg << "RobotiqController subscribing to twist messages on [" << topic << "]" << std::endl;
-
+    this->dataPtr->gripperStatus = GripperStatus::move_close;
 }
 
 void RobotiqController::PreUpdate(const ignition::gazebo::UpdateInfo &_info,
-                             ignition::gazebo::EntityComponentManager &_ecm)
-{
+                                  ignition::gazebo::EntityComponentManager &_ecm) {
+    //do nothing if it's paused or not initialized.
+    if (_info.paused) {
+        return;
+    }
     //current state
     GripperStatus gripperStatus;
     {
         std::lock_guard<std::mutex> lock(this->dataPtr->statusMutex);
         gripperStatus = this->dataPtr->gripperStatus;
     }
-    //current state
-    //const auto chassisPose = _ecm.Component<components::WorldPose>(this->dataPtr->chassisLink)->Data();
-
-    //for angular velocity control
-    //double wErr = angularVel.Z() - targetVel.angular().z();
-    //double wCmd = this->dataPtr->wPid.Update(wErr, _info.dt);
-
+    //FSM
+    if (gripperStatus == GripperStatus::move_close) {
+        //check and set velocity
+        if (this->dataPtr->checkGrasp(_ecm)) {
+            std::lock_guard<std::mutex> lock(this->dataPtr->statusMutex);
+            this->dataPtr->gripperStatus = GripperStatus::keep_close;
+            this->dataPtr->setJointVelocity(_ecm, 0);
+        } else {
+            this->dataPtr->setJointVelocity(_ecm, 0.1);
+        }
+    } else if (gripperStatus == GripperStatus::move_open) {
+        //check and set velocity
+        if (this->dataPtr->getJointPosition(_ecm) < 0.001) {
+            std::lock_guard<std::mutex> lock(this->dataPtr->statusMutex);
+            this->dataPtr->gripperStatus = GripperStatus::keep_open;
+            this->dataPtr->setJointVelocity(_ecm, 0);
+        } else {
+            this->dataPtr->setJointVelocity(_ecm, -0.1);
+        }
+    } else if (gripperStatus == GripperStatus::keep_open || gripperStatus == GripperStatus::keep_close) {
+        this->dataPtr->setJointVelocity(_ecm, 0);
+    }
 }
-void RobotiqController::PostUpdate(const ignition::gazebo::UpdateInfo &_info,
-                              const ignition::gazebo::EntityComponentManager &_ecm)
-{
+void RobotiqController::PostUpdate(const ignition::gazebo::UpdateInfo &/*_info*/,
+                                   const ignition::gazebo::EntityComponentManager &/*_ecm*/) {
     //do nothing
 }
 
 /******************implementation for RobotiqControllerPrivate******************/
+void RobotiqControllerPrivate::setJointVelocity(ignition::gazebo::EntityComponentManager &_ecm, double vel) {
+    for (size_t i = 0; i < this->jointEntities.size(); i++) {
+        auto jointVelCmd = _ecm.Component<components::JointVelocityCmd>(this->jointEntities[i]);
+        double jointVel = vel * this->jointMultipliers[i];
+        if (jointVelCmd == nullptr) {
+            _ecm.CreateComponent(this->jointEntities[i], components::JointVelocityCmd({jointVel}));
+        } else {
+            jointVelCmd->Data()[0] = jointVel;
+        }
+    }
+}
 
-void RobotiqControllerPrivate::OnCmd(const ignition::msgs::Boolean  &_msg)
-{
+double RobotiqControllerPrivate::getJointPosition(ignition::gazebo::EntityComponentManager &_ecm) {
+    auto jointPosComp = _ecm.Component<components::JointPosition>(this->jointEntities[0]);
+    if (jointPosComp == nullptr) {
+        _ecm.CreateComponent(this->jointEntities[0], components::JointPosition());
+    }
+    return fabs(jointPosComp->Data().at(0));
+}
+
+bool RobotiqControllerPrivate::checkGrasp(ignition::gazebo::EntityComponentManager &_ecm) {
+    auto base = getJointPosition(_ecm);
+    if (base > 0.695) {
+        ignmsg << "Grasp Nothing!" << std::endl;
+        return true;
+    }
+    double diff = 0;
+    for (size_t i = 0; i < this->jointEntities.size(); i++) {
+        auto jointPosComp = _ecm.Component<components::JointPosition>(this->jointEntities[i]);
+        if (jointPosComp == nullptr) {
+            _ecm.CreateComponent(this->jointEntities[0], components::JointPosition());
+        }
+        auto pos = fabs(jointPosComp->Data().at(0));
+        diff = diff + fabs(pos - base);
+    }
+    if(diff > 0.02){
+        ignmsg << "Grasp Object!" << std::endl;
+        return true;
+    }
+    return false;
+}
+
+void RobotiqControllerPrivate::OnCmd(const ignition::msgs::Boolean &_msg) {
     std::lock_guard<std::mutex> lock(this->statusMutex);
-
     //is grasp?
-    if (_msg.data()){
+    if (_msg.data()) {
         //to close gripper (grasp it!)
         if (this->gripperStatus == GripperStatus::keep_open || this->gripperStatus == GripperStatus::move_open) {
             this->gripperStatus = GripperStatus::move_close;
-            //last_check_time_ = model_->GetWorld()->SimTime();
-            //last_check_position_ = joints_[0]->Position();
-            //InitPidControl();
         }
     } else {
         //to open gripper (don't grasp!)
         if (this->gripperStatus == GripperStatus::keep_close || this->gripperStatus == GripperStatus::move_close) {
             this->gripperStatus = GripperStatus::move_open;
-            //last_check_time_ = model_->GetWorld()->SimTime();
-            //InitPidControl();
         }
     }
 }
-
 
 /******************register*************************************************/
 IGNITION_ADD_PLUGIN(RobotiqController,
